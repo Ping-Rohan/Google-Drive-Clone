392da9cfc667b1b3f5ab5ea4bb285900172580a5
Rohan Tiwari <ping.techyrohan@gmail.com>
2023-06-01 23:29:35 +0545

    typo fix

--- frontend/src/Components/FileComponent.jsx
+++ frontend/src/Components/FileComponent.jsx
@@ -5,7 +5,7 @@ export default function FileComponent() {
   return (
     <div className="bg-slate-900 w-[200px] h-[150px] text-white px-3 py-2 flex flex-col items-center justify-center gap-2 font-josefin">
       <FaFileVideo size={90} />
-      Erick.mp4
+      TestFile.mp4
     </div>
   );
 }
